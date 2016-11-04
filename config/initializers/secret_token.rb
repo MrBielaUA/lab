1c9072c8a5fc7fb3bfe7de9e090bbe31133122f0
MrBielaUA <fifafan@mail.ua>
2016-11-04 10:21:44 +0200

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appname::Application.config.secret_token = 'f9db0936e5b7b721a7e0e10378e03a9c87149c0a23564439f6e8712a8ed868ef6a9e4838fdf8c762e8dc53a9020de1df84bd6b3c001b444c2cd4eceaf6dcb231'
